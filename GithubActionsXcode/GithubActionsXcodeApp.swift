dc9d74581fda7d44677aa3156df1d66ad21894ca
M Shahid Shakeel <m.shahidshakeel@gmail.com>
2023-09-10 16:52:14 +0500

    init

---
+++ GithubActionsXcode/GithubActionsXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsXcodeApp.swift
+//  GithubActionsXcode
+//
+//  Created by M Shahid Shakeel on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
